5833e3d9c087963c92bb9eeeeb6a76cf8e6736ec
raphachegeke <raphachegekamunu@gmail.com>
2025-09-04 16:12:22 +0300

    added div.html: like adding items in a list

--- exercisedom.js
+++ exercisedom.js
@@ -3,19 +3,21 @@ let myName = "Rapha Chege";
 
 // takes the first[0] element with a class name heading 
 let heading = document.getElementsByClassName("heading")[0];
+let heading1 = document.getElementsByClassName("heading")[0];
+
 
 // takes the first[0] element with a class name paragraph
 let paragraph = document.getElementsByClassName("paragraph")[0];
 
 // trigger when mouse goes over the heading
 heading.addEventListener("mouseover", () => {
-    heading.outerHTML = '<p style="color: green;">Welcome to FCA</p>';
+    heading.innerHTML = '<p style="color: green;">Welcome to FCA</p>';
 });
 
 // // reset text when mouse leaves
-// heading.addEventListener("mouseout", () => {
-//     heading.outerHTML = '<h1 class="heading">Hello there✌️</h1>'
-// });
+heading1.addEventListener("click", () => {
+    heading1.innerHTML = '<h1 class="heading">Hello there✌️</h1>'
+});
 paragraph.addEventListener("click", () => {
         paragraph.outerHTML = '<h1 style="color: yellow;">My name is ' + myName + '</h1>'
 
